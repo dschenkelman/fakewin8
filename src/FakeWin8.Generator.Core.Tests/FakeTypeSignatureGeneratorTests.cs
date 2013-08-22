51ae63b5edae1a76a588f5f47aad962c4332de26
dschenkelman <damian.schenkelman@gmail.com>
2013-08-21 22:31:30 -0300

    # Initial support for classes with generic parameters

--- src/FakeWin8.Generator.Core.Tests/FakeTypeSignatureGeneratorTests.cs
+++ src/FakeWin8.Generator.Core.Tests/FakeTypeSignatureGeneratorTests.cs
@@ -9,7 +9,31 @@
         {
         }
 
+        private interface IGenericInterface<T>
+        {
+        }
+
+        private interface IGenericInterfaceWithDefaultConstructorConstraint<T> where T : new()
+        {
+        }
+
+        private interface IGenericInterfaceWithClassConstraint<T> where T : class
+        {
+        }
+
+        private interface IGenericInterfaceWithStructConstraint<T> where T : struct
+        {
+        }
+
+        private interface IGenericInterfaceWithOtherArgumentConstraint<T, U> where T : U
+        {
+        }
+
         private abstract class AbstractClass
+        {
+        }
+        
+        private abstract class AbstractGenericClass<T>
         {
         }
 
@@ -31,6 +55,66 @@
             var signature = generator.Generate();
 
             Assert.AreEqual("public class FakeAbstractClass : AbstractClass", signature);
+        }
+
+        [TestMethod]
+        public void ShouldGenerateFakeTypeSignatureForGenericInterfaceFake()
+        {
+            var generator = new FakeTypeSignatureGenerator(typeof(IGenericInterface<>));
+
+            var signature = generator.Generate();
+
+            Assert.AreEqual("public class FakeGenericInterface<T> : IGenericInterface<T>", signature);
+        }
+
+        [TestMethod]
+        public void ShouldGenerateFakeTypeSignatureForAbstractGenericClassFake()
+        {
+            var generator = new FakeTypeSignatureGenerator(typeof(AbstractGenericClass<>));
+
+            var signature = generator.Generate();
+
+            Assert.AreEqual("public class FakeAbstractGenericClass<T> : AbstractGenericClass<T>", signature);
+        }
+
+        [TestMethod]
+        public void ShouldGenerateFakeTypeSignatureForGenericInterfaceWithDefaultConstructorTypeConstraint()
+        {
+            var generator = new FakeTypeSignatureGenerator(typeof(IGenericInterfaceWithDefaultConstructorConstraint<>));
+
+            var signature = generator.Generate();
+
+            Assert.AreEqual("public class FakeGenericInterfaceWithDefaultConstructorConstraint<T> : IGenericInterfaceWithDefaultConstructorConstraint<T> where T : new()", signature);
+        }
+
+        [TestMethod]
+        public void ShouldGenerateFakeTypeSignatureForGenericInterfaceWithClassTypeConstraint()
+        {
+            var generator = new FakeTypeSignatureGenerator(typeof(IGenericInterfaceWithClassConstraint<>));
+
+            var signature = generator.Generate();
+
+            Assert.AreEqual("public class FakeGenericInterfaceWithClassConstraint<T> : IGenericInterfaceWithClassConstraint<T> where T : class", signature);
+        }
+
+        [TestMethod]
+        public void ShouldGenerateFakeTypeSignatureForGenericInterfaceWithStructTypeConstraint()
+        {
+            var generator = new FakeTypeSignatureGenerator(typeof(IGenericInterfaceWithStructConstraint<>));
+
+            var signature = generator.Generate();
+
+            Assert.AreEqual("public class FakeGenericInterfaceWithStructConstraint<T> : IGenericInterfaceWithStructConstraint<T> where T : struct", signature);
+        }
+
+        [TestMethod]
+        public void ShouldGenerateFakeTypeSignatureForGenericInterfaceWithOtherArgumentTypeConstraint()
+        {
+            var generator = new FakeTypeSignatureGenerator(typeof(IGenericInterfaceWithOtherArgumentConstraint<,>));
+
+            var signature = generator.Generate();
+
+            Assert.AreEqual("public class FakeGenericInterfaceWithOtherArgumentConstraint<T, U> : IGenericInterfaceWithOtherArgumentConstraint<T, U> where T : U", signature);
         }
     }
 }
--- src/FakeWin8.Generator/FakeTypeSignatureGenerator.cs
+++ src/FakeWin8.Generator/FakeTypeSignatureGenerator.cs
@@ -1,9 +1,14 @@
 ﻿namespace FakeWin8.Generator.Core
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
 
     public class FakeTypeSignatureGenerator
     {
+        private const string ParameterConstraintFormat = "where {0} : {1}";
+
         private readonly Type type;
 
         public FakeTypeSignatureGenerator(Type type)
@@ -28,7 +33,62 @@
 
             trimmedName = GetFakeTypeName(this.type, trimmedName);
 
-            return string.Format("public class Fake{0} : {1}", trimmedName, typeGenerator.Generate());
+            var constraints = this.GetConstraints();
+
+            var signature = string.Format("public class Fake{0} : {1}", trimmedName, typeGenerator.Generate());
+
+            if (constraints.Count != 0)
+            {
+                signature += " " + string.Join(",", constraints.ToArray());
+            }
+
+            return signature;
+        }
+
+        private List<string> GetConstraints()
+        {
+            var constraints = new List<string>();
+
+            foreach (var argument in this.type.GetGenericArguments())
+            {
+                var genericArgumentName = argument.Name;
+
+                string nonTypedConstraint = GetNonTypedConstraint(argument, genericArgumentName);
+
+                if (nonTypedConstraint != null)
+                {
+                    constraints.Add(nonTypedConstraint);
+                }
+
+                constraints.AddRange(argument
+                    .GetGenericParameterConstraints().Where(t => t.Name != "ValueType")
+                    .Select(constraintType => string.Format(
+                        ParameterConstraintFormat, 
+                        genericArgumentName, 
+                        new TypeGenerator(constraintType).Generate())));
+            }
+
+            return constraints;
+        }
+
+        private static string GetNonTypedConstraint(Type argument, string genericArgumentName)
+        {
+            if ((argument.GenericParameterAttributes & GenericParameterAttributes.ReferenceTypeConstraint) != 0)
+            {
+                return string.Format(ParameterConstraintFormat, genericArgumentName, "class");
+            }
+            
+            if ((argument.GenericParameterAttributes & GenericParameterAttributes.NotNullableValueTypeConstraint) != 0)
+            {
+                return string.Format(ParameterConstraintFormat, genericArgumentName, "struct");
+            }
+            
+            if ((argument.GenericParameterAttributes & GenericParameterAttributes.DefaultConstructorConstraint) != 0)
+            {
+                return string.Format(ParameterConstraintFormat, genericArgumentName, "new()");
+            }
+
+            return null;
         }
     }
 }
